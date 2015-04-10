75482fdee510cb105cf5b5705bf2a083e7572c59
Jacob Garbe <logodaedalus@gmail.com>
2015-04-09 17:16:59 -0700

    Greedy bot working!

--- greedy_bot.py
+++ greedy_bot.py
@@ -1,8 +1,12 @@
+import random
+
 def think(state, quip):
    bestScore = state.get_score()
    moves = state.get_moves()
+   bestMove = random.choice(state.get_moves())
    for move in moves:
-      attempt = state.copy().apply_move(move)
+      attempt = state.copy()
+      attempt.apply_move(move)
       if attempt.get_score() > bestScore:
          bestMove = move
          bestScore = attempt.get_score()
