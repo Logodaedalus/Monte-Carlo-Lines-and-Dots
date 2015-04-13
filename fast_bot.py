36c5c24a030fa79d2e89f8b6ee38c355c96e1414
tmlinejo <tmlinejo@ucsc.edu>
2015-04-13 08:15:13 -0700

    corrected iteration depth

--- fast_bot.py
+++ fast_bot.py
@@ -1,7 +1,7 @@
 import bot_core
 
 def think(state, quip):
-    x = bot_core.UCT(rootstate = state, iterdepth = 5, itertime = 1, verbose = True)
+    x = bot_core.UCT(rootstate = state, iterdepth = 4, itertime = 1, verbose = True)
     output = "I'm quickly choosing " + str(x)
     quip(output)
     return x
