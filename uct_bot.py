ee9a3ee4c452c5bc00e59c9444cbd2b433d9bce7
tmlinejo <tmlinejo@ucsc.edu>
2015-04-13 08:30:41 -0700

    fixed quip

--- uct_bot.py
+++ uct_bot.py
@@ -2,5 +2,5 @@ import bot_core
 
 def think(state, quip):
   theMove = bot_core.UCT(state, 999999999, 1, verbose = True)
-  quip("I'm strategically choosing ", theMove)
+  quip("I'm strategically choosing " + str(theMove))
   return theMove
